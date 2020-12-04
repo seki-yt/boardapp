ed0be147b9ece8842469adcf533b979c82fa791a
seki_yuts <>
2020-12-04 21:42:52 +0900

    deleteアクション作成

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   root to: "boards#index"
 
-  resources :boards, only: [:show, :new, :create, :edit, :update]
+  resources :boards, only: [:show, :new, :create, :edit, :update, :destroy]
 end
