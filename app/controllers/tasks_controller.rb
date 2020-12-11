3e00b6cadac090ee83d307ca9a1c292d76ce570d
seki_yuts <>
2020-12-11 23:14:25 +0900

    taskとboardの紐付け

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,6 +15,40 @@ class TasksController < ApplicationController
     end
   end
 
+  def show
+    @board = Board.find(params[:board_id])
+    @task = @board.tasks.find(params[:id])
+     # =>  /boards/:board_id/tasks/:id(:format)
+     # => idが2つ使えるようになる
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+    @board = Board.find(params[:board_id])
+  end
+
+  def update
+    @board = Board.find(params[:board_id])
+    @task = @board.tasks.find(params[:id])
+
+
+    if @task.update(task_params)
+      redirect_to board_task_path(@board.id, @task.id)
+    else
+      render new
+    end
+  end
+
+  def destroy
+    @board = Board.find(params[:board_id])
+    @task = @board.tasks.find(params[:id])
+
+    @task.destroy!
+    redirect_to board_path(@board)
+
+  end
+
+
   private
   def task_params
     params.require(:task).permit(:title, :content, :eyecatch).merge(user_id: current_user.id )
